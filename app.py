54aed62dc97bd99476c36e1102ff7e4b8f5f5f2b
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-04-19 09:21:09 -0300

    Se cambio el metodo de verificacion de dato de entrada

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 def analisis_estadistico(l: list):
+    
+    if not isinstance(l, list):
+        raise TypeError('El argumento debe ser una lista')
+    
     # Comprueba si la lista está vacía
     if len(l) == 0:
         raise ValueError('La lista debe tener al menos un elemento')
@@ -9,45 +13,40 @@ def analisis_estadistico(l: list):
     if not isinstance(l[0], int):
         raise TypeError('Los elementos de la lista deben ser numeros')
     
-    try:
-        # Ordena la lista para facilitar el cálculo
-        l.sort()
-        
-        # Crea un diccionario para almacenar la frecuencia de cada elemento
-        fi = {}
-        for i in l:
-            if i in fi:
-                fi[i] += 1
-            else:
-                fi[i] = 1
+    # Ordena la lista para facilitar el cálculo
+    l.sort()
+    
+    # Crea un diccionario para almacenar la frecuencia de cada elemento
+    fi = {}
+    for i in l:
+        if i in fi:
+            fi[i] += 1
+        else:
+            fi[i] = 1
 
-        # Extrae las claves y los valores del diccionario
-        edades_keys = fi.keys()
-        edades_values = fi.values()
+    # Extrae las claves y los valores del diccionario
+    edades_keys = fi.keys()
+    edades_values = fi.values()
 
-        # Crea un DataFrame de pandas a partir de las claves y valores del diccionario
-        df = pd.DataFrame({'Edades': edades_keys, 'fi': edades_values})
-        
-        # Calcula la frecuencia acumulada
-        df['Fi'] = df['fi'].cumsum()
-        
-        # Calcula la frecuencia relativa
-        df['ri'] = df['fi'] / df['fi'].sum()
-        
-        # Calcula la frecuencia relativa acumulada
-        df['Ri'] = df['ri'].cumsum()
-        
-        # Calcula los porcentajes de frecuencia relativa
-        df['pi%'] = df['ri'] * 100
-        
-        # Calcula los porcentajes de frecuencia relativa acumulada
-        df['Pi%'] = df['pi%'].cumsum()
+    # Crea un DataFrame de pandas a partir de las claves y valores del diccionario
+    df = pd.DataFrame({'Edades': edades_keys, 'fi': edades_values})
+    
+    # Calcula la frecuencia acumulada
+    df['Fi'] = df['fi'].cumsum()
+    
+    # Calcula la frecuencia relativa
+    df['ri'] = df['fi'] / df['fi'].sum()
+    
+    # Calcula la frecuencia relativa acumulada
+    df['Ri'] = df['ri'].cumsum()
+    
+    # Calcula los porcentajes de frecuencia relativa
+    df['pi%'] = df['ri'] * 100
+    
+    # Calcula los porcentajes de frecuencia relativa acumulada
+    df['Pi%'] = df['pi%'].cumsum()
 
-        return df
-    except TypeError:
-        print('Error en el tipo de dato')
-    except ValueError:
-        print('Error en el valor')
+    return df
 
 # Ejemplo de uso
 l = [19, 29, 19, 22, 23, 19, 30, 19, 19, 19, 20, 20, 20, 18, 22, 19, 34, 34, 21,  21, 22, 28,  29, 19, 20, 19, 25, 28, 21, 22]
